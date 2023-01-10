cf62a5f92a4515dd825072fe32d080377803e042
Michael Vargas <vacomi.corp@gmail.com>
2023-01-09 22:31:58 -0500

    session 13 finalizado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,39 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'session13';
+  textoupper = '';
+  dinero = '';
+  seleccionado = 'vacio';
+  
+  // Ejercicio 03
+  fecha = '';
+  fa!:Date;
+  tiempo = '';
+
+  calcular():void {
+    if(!this.fecha) {
+      this.fecha = new Date().toLocaleString();
+    }
+
+    this.fa = new Date();
+    let fecha2 = new Date(this.fecha).getTime();
+    let numero = this.fa.getTime() - fecha2;
+
+    // Dias
+    if(numero >= 86400000) {
+      let dias  = Math.floor(numero / 86400000);
+      this.tiempo = `Hace ${dias} dias.`
+    }else if(3600000 <=numero) {
+      let hrs = Math.floor(numero / 3600000);
+      this.tiempo = `Hace ${hrs} horas.`
+    }else if(60000 <= numero) {
+      let min  = Math.floor(numero / 60000);
+      this.tiempo = `Hace ${min} minutos.`
+    }else {
+      let sec  = Math.floor(numero / 1000);
+      this.tiempo = `Hace ${sec} segundos.`
+    }
+
+  }
+
 }
